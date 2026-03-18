c619a3495eba2c351d5d91a1c9533169da628f8f
yourMomLastNight <notagainoldman@gmail.com>
2026-03-18 12:43:18 +0100

    Made the IMUState struct have a sequential layout.

--- Runtime/InputStates/IMUState.cs
+++ Runtime/InputStates/IMUState.cs
@@ -2,17 +2,22 @@ using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.InputSystem.Utilities;
 using UnityEngine.InputSystem.Layouts;
+using System.Runtime.InteropServices;
 
 namespace MoreStories.GyroTools
 {
+    [StructLayout(LayoutKind.Explicit)]
     public struct IMUState : IInputStateTypeInfo
     {
-        public FourCC format => new FourCC('I', 'M', 'U', 'S');
+        public static FourCC Format {get; private set;} = new FourCC('I', 'M', 'U', 'S');
+        public FourCC format => Format;
 
         [InputControl(name = "accel", layout = "Vector3", usage = "Acceleration",    displayName = "Accelerometer")]
+        [FieldOffset(0)]
         public Vector3 accelerometer;
 
         [InputControl(name = "gyro",  layout = "Vector3", usage = "AngularVelocity", displayName = "Gyroscope")]
+        [FieldOffset(12)]
         public Vector3 gyroscope;
     }
 }
